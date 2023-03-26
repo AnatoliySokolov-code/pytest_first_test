c63b1371a97c2c023ffd2a17dc46aa9dd01f18ca
AnatoliySokolov-code <109688326+AnatoliySokolov-code@users.noreply.github.com>
2023-03-26 13:11:49 +0300

    Создал 4 положительных теста и переименовал проект

--- tests/first_tests.py
+++ tests/first_tests.py
@@ -7,7 +7,13 @@ class TestCalc:
         self.calc = Calculator
 
     def test_multiply_calculate_correctly(self):
-        assert self.calc.multiply(self, 2, 2) == 4
+        assert self.calc.multiply(self, 3, 3) == 9
 
-    def test_multiply_calculate_failed(self):
-        assert self.calc.multiply(self, 2, 2) == 5
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 2, 2) == 1
+
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 5, 1) == 4
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 9, 1) == 10
